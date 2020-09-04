e22cc9b07182cd853672c88de87f3b0d86304d22
vivek-ng <vivek@shipt.com>
2020-09-04 17:53:28 -0500

    initial impl for subscribers

---
+++ pubsub/pubsub.go
@@ -0,0 +1,36 @@
+package pubsub
+
+import "sync"
+
+type (
+	Data struct {
+		Message interface{}
+	}
+
+	DataChan chan Data
+
+	PubSub struct {
+		capacity    int
+		subscribers map[string][]DataChan
+		mux         sync.RWMutex
+	}
+)
+
+func NewPubSub(capacity int) *PubSub {
+	return &PubSub{
+		capacity: capacity,
+	}
+}
+
+func (pb *PubSub) Subscribe(topic string) DataChan {
+	pb.mux.Lock()
+	defer pb.mux.Unlock()
+	ch := make(DataChan)
+	_, ok := pb.subscribers[topic]
+	if ok {
+		pb.subscribers[topic] = append(pb.subscribers[topic], ch)
+	} else {
+		pb.subscribers[topic] = append([]DataChan{}, ch)
+	}
+	return ch
+}
